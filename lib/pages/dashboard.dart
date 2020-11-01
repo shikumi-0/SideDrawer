8997fe2f19de43d7d5fac19b309190619528a256
ShiKuMi <49393139+shikumi-0@users.noreply.github.com>
2020-11-01 23:59:48 +0530

    Update dashboard.dart

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -5,12 +5,13 @@ class DashboardPage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffD6ED17),
       body: Center(
         child: Text(
           'Dashboard',
           style: TextStyle(
             fontSize: 30,
-            color: Colors.black,
+            color: Color(0xff606060),
             fontWeight: FontWeight.w900,
           ),
         ),
