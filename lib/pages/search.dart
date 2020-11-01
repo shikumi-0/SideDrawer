04a1435806fc043981b5de51a9aca6df9b22407d
ShiKuMi <49393139+shikumi-0@users.noreply.github.com>
2020-11-02 00:07:19 +0530

    Update search.dart

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -5,12 +5,13 @@ class SearchPage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffC7D3D4),
       body: Center(
         child: Text(
           'Search',
           style: TextStyle(
             fontSize: 30,
-            color: Colors.black,
+            color: Color(0xff603F83),
             fontWeight: FontWeight.w900,
           ),
         ),
