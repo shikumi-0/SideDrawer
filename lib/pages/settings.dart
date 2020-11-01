b2dfa9e8d256ca416daee1b2784717568efd3d34
ShiKuMi <49393139+shikumi-0@users.noreply.github.com>
2020-11-02 00:07:15 +0530

    Update settings.dart

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -5,12 +5,13 @@ class SettingsPage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffCDB599),
       body: Center(
         child: Text(
           'Settings',
           style: TextStyle(
             fontSize: 30,
-            color: Colors.black,
+            color: Color(0xff42EADD),
             fontWeight: FontWeight.w900,
           ),
         ),
